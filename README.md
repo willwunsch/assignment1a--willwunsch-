27355ff111e8bf904cfb6c209287eff375e69a46
willwunsch <127810234+willwunsch@users.noreply.github.com>
2024-09-12 22:50:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# assignment1a-Rafi-ur-Rashid
---
+++ write_ids.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
+    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+
+    return 0 ;
+}
